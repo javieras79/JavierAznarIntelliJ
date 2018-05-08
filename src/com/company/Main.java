6a074d24cfb33bb83ba0a88a8ab81abe3cb1b417
javieras79 <javieras79@gmail.com>
2018-05-09 00:11:52 +0200

    Commit cambio metodo y comentarios Javadoc rama veredicto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,17 @@ package com.company;
 
 public class Main {
 
-    public static String veredicto(String valor1, String valor2){
-
+    public static void main(String[] args) {
+        System.out.println("La calificación de " + args[0] + ":" + args[1]);
+        System.out.println(veredicto(args[1],args[2]));
+    }
+    /**
+     * compara dos valores y devuelve mensaje
+     * @param valor1 nota que creo que voy a sacar
+     * @param valor2 nota real del curso
+     * @return mensaje devuelto según comparacion de ambas notas
+     */
+    private static String veredicto(String valor1, String valor2){
         if(valor1.equals(valor2)){
             return "Perfecto. Has tenido buen criterio.";
         }
@@ -12,8 +21,4 @@ public class Main {
         }
         return "Te has confiado. Falta realismo.";    }
 
-    public static void main(String[] args) {
-        System.out.println("La calificación de " + args[0] + ":" + args[1]);
-        System.out.println(veredicto(args[1],args[2]));
-    }
 }
